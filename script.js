a0ec22da22cec607efa599ad8c8452f5f1c7ef3a
lei1313 <77952651+lei1313@users.noreply.github.com>
2021-03-12 15:32:19 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstname = "Lei";
+var lastname = " Chen";
+var age = 20;
+alert ("My mame is " + firstname + lastname + ".")
+alert ("I am " + age + " years old.")
+console.log("I am have a lot of fun in this class");
